d6fe065b23980485274c2d771b15f734109ee3c0
sulissskuuu <sulissskuuu@gmail.com>
2024-12-11 05:43:11 +0000

    add new feature edit todo & add alert notification delete todo

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import '../model/todo.dart';
 import '../constants/colors.dart';
+import '../model/todo.dart';
 import '../widgets/todo_item.dart';
 
 class Home extends StatefulWidget {
@@ -12,14 +14,14 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final todosList = ToDo.todoList();
+  List<ToDo> todosList = [];
   List<ToDo> _foundToDo = [];
-  final _todoController = TextEditingController();
+  final TextEditingController _todoController = TextEditingController();
 
   @override
   void initState() {
-    _foundToDo = todosList;
     super.initState();
+    _loadToDos();
   }
 
   @override
@@ -27,104 +29,93 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Stack(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 15,
-            ),
-            child: Column(
-              children: [
-                searchBox(),
-                Expanded(
-                  child: ListView(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(
-                          top: 50,
-                          bottom: 20,
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            searchBox(),
+            Expanded(
+              child: _foundToDo.isEmpty
+                  ? Center(
+                      child: const Text(
+                        'No ToDos found',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: tdGrey,
                         ),
-                        child: const Text(
-                          'All ToDos',
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.w500,
+                      ),
+                    )
+                  : ListView(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 20, bottom: 20),
+                          child: const Text(
+                            'All ToDos',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
-                      ),
-                      for (ToDo todoo in _foundToDo.reversed)
-                        ToDoItem(
-                          todo: todoo,
-                          onToDoChanged: _handleToDoChange,
-                          onDeleteItem: _deleteToDoItem,
-                        ),
-                    ],
-                  ),
-                )
-              ],
+                        for (ToDo todo in _foundToDo.reversed)
+                          ToDoItem(
+                            todo: todo,
+                            onToDoChanged: _handleToDoChange,
+                            onDeleteItem: _showDeleteConfirmationDialog,
+                            onEditItem: _editToDoItem,
+                          ),
+                      ],
+                    ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Row(children: [
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(
-                    bottom: 20,
-                    right: 20,
-                    left: 20,
+            // Add New ToDo Section
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(0.0, 0.0),
+                    blurRadius: 10.0,
+                    spreadRadius: 0.0,
                   ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 5,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Colors.grey,
-                        offset: Offset(0.0, 0.0),
-                        blurRadius: 10.0,
-                        spreadRadius: 0.0,
-                      ),
-                    ],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: TextField(
-                    controller: _todoController,
-                    decoration: const InputDecoration(
-                        hintText: 'Add a new todo item',
-                        border: InputBorder.none),
-                  ),
-                ),
+                ],
+                borderRadius: BorderRadius.circular(10),
               ),
-              Container(
-                margin: const EdgeInsets.only(
-                  bottom: 20,
-                  right: 20,
-                ),
-                child: ElevatedButton(
-                  onPressed: () {
-                    _addToDoItem(_todoController.text);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: tdBlue,
-                    minimumSize: const Size(60, 60),
-                    elevation: 10,
-                  ),
-                  child: const Text(
-                    '+',
-                    style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _todoController,
+                      decoration: const InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
-                ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _addToDoItem(_todoController.text);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: tdBlue,
+                      minimumSize: const Size(60, 60),
+                      elevation: 10,
+                    ),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ]),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -133,27 +124,82 @@ class _HomeState extends State<Home> {
     setState(() {
       todo.isDone = !todo.isDone;
     });
+    _saveToDos();
   }
 
   void _deleteToDoItem(String id) {
     setState(() {
       todosList.removeWhere((item) => item.id == id);
+      _foundToDo = todosList;
     });
+    _saveToDos();
   }
 
   void _addToDoItem(String toDo) {
+    if (toDo.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('ToDo cannot be empty!')),
+      );
+      return;
+    }
+
     setState(() {
       todosList.add(ToDo(
         id: DateTime.now().millisecondsSinceEpoch.toString(),
         todoText: toDo,
       ));
+      _foundToDo = todosList;
     });
     _todoController.clear();
+    _saveToDos();
+  }
+
+  void _editToDoItem(ToDo todo) {
+    final TextEditingController _editController =
+        TextEditingController(text: todo.todoText);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Edit ToDo'),
+          content: TextField(
+            controller: _editController,
+            decoration: const InputDecoration(
+              hintText: 'Update your todo',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_editController.text.trim().isEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('ToDo cannot be empty!')),
+                  );
+                  return;
+                }
+
+                setState(() {
+                  todo.todoText = _editController.text;
+                });
+                Navigator.of(context).pop();
+                _saveToDos();
+              },
+              child: const Text('Save'),
+            ),
+          ],
+        );
+      },
+    ).then((_) => _editController.dispose());
   }
 
   void _runFilter(String enteredKeyword) {
-    List<ToDo> results = [];
-    if (enteredKeyword.isEmpty) {
+    List<ToDo> results;
+    if (enteredKeyword.trim().isEmpty) {
       results = todosList;
     } else {
       results = todosList
@@ -168,6 +214,25 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<void> _loadToDos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? todosString = prefs.getString('todos');
+    if (todosString != null) {
+      final List<dynamic> todosJson = json.decode(todosString);
+      setState(() {
+        todosList = todosJson.map((json) => ToDo.fromJson(json)).toList();
+        _foundToDo = todosList;
+      });
+    }
+  }
+
+  Future<void> _saveToDos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String todosString =
+        json.encode(todosList.map((todo) => todo.toJson()).toList());
+    await prefs.setString('todos', todosString);
+  }
+
   Widget searchBox() {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 15),
@@ -176,18 +241,11 @@ class _HomeState extends State<Home> {
         borderRadius: BorderRadius.circular(20),
       ),
       child: TextField(
-        onChanged: (value) => _runFilter(value),
+        onChanged: _runFilter,
         decoration: const InputDecoration(
           contentPadding: EdgeInsets.all(0),
-          prefixIcon: Icon(
-            Icons.search,
-            color: tdBlack,
-            size: 20,
-          ),
-          prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 25,
-          ),
+          prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+          prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
           border: InputBorder.none,
           hintText: 'Search',
           hintStyle: TextStyle(color: tdGrey),
@@ -200,24 +258,50 @@ class _HomeState extends State<Home> {
     return AppBar(
       backgroundColor: tdBGColor,
       elevation: 0,
-      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        const Text(
-          'Notes App', // Teks yang muncul di sebelah ikon
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-            color: tdBlack, // Sesuaikan warna teks jika perlu
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Text(
+            'Notes App',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.w500, color: tdBlack),
           ),
-        ),
-        SizedBox(
-          height: 40,
-          width: 40,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset('assets/images/avatar.jpeg'),
+          SizedBox(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/furina.jpeg'),
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
+    );
+  }
+
+  // Show delete confirmation dialog
+  void _showDeleteConfirmationDialog(String id) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this ToDo?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _deleteToDoItem(id);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -5,14 +5,16 @@ import '../constants/colors.dart';
 
 class ToDoItem extends StatelessWidget {
   final ToDo todo;
-  final onToDoChanged;
-  final onDeleteItem;
+  final ValueChanged<ToDo> onToDoChanged;
+  final ValueChanged<String> onDeleteItem;
+  final ValueChanged<ToDo> onEditItem;
 
   const ToDoItem({
     Key? key,
     required this.todo,
     required this.onToDoChanged,
     required this.onDeleteItem,
+    required this.onEditItem,
   }) : super(key: key);
 
   @override
@@ -20,45 +22,40 @@ class ToDoItem extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.only(bottom: 20),
       child: ListTile(
-        onTap: () {
-          // print('Clicked on Todo Item.');
-          onToDoChanged(todo);
-        },
+        onTap: () => onToDoChanged(todo),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
         ),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
           todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
           color: tdBlue,
         ),
         title: Text(
-          todo.todoText!,
+          todo.todoText ?? '',
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
-            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+            decoration:
+                todo.isDone ? TextDecoration.lineThrough : TextDecoration.none,
           ),
         ),
-        trailing: Container(
-          padding: const EdgeInsets.all(0),
-          margin: const EdgeInsets.symmetric(vertical: 12),
-          height: 35,
-          width: 35,
-          decoration: BoxDecoration(
-            color: tdRed,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: IconButton(
-            color: Colors.white,
-            iconSize: 18,
-            icon: const Icon(Icons.delete),
-            onPressed: () {
-              // print('Clicked on delete icon');
-              onDeleteItem(todo.id);
-            },
-          ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.edit, color: tdBlue),
+              tooltip: 'Edit ToDo',
+              onPressed: () => onEditItem(todo),
+            ),
+            IconButton(
+              icon: const Icon(Icons.delete, color: tdRed),
+              tooltip: 'Delete ToDo',
+              onPressed: () => onDeleteItem(todo.id!),
+            ),
+          ],
         ),
       ),
     );
